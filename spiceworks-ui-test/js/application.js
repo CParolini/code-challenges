b27de6fc5cb22260a69eb21c41f74150e623553d
Chris Parolini <chris.parolini@chatid.com>
2016-12-27 12:09:10 -0600

    adding all changes made thus far for Spiceworks challenge

---
+++ spiceworks-ui-test/js/application.js
@@ -0,0 +1,34 @@
+// Here's the initial data. Again: don't worry about persistence :)
+var data = [{
+    name: "Mark-Paul Gosselaar",
+    photo_url: ""
+}, {
+    name: "Delta Burke",
+    photo_url: "img/avatars/delta.png"
+}, {
+    name: "Alf",
+    photo_url: "img/avatars/alf.png"
+}, {
+    name: "Jaleel White",
+    photo_url: "img/avatars/jaleel.png"
+}, {
+    name: "Ralph Macchio",
+    photo_url: "img/avatars/ralph.png"
+}, {
+    name: "Candace Cameron",
+    photo_url: "img/avatars/candace.png"
+}, {
+    name: "Patrick Duffy",
+    photo_url: "img/avatars/pduff.png"
+}, {
+    name: "Arnold Schwartzengger",
+    photo_url: "img/avatars/arnold.png"
+}];
+
+// function display() {
+//     for (var i = 0; i < data.length; i++) {
+//       console.log(data[i]);
+//       $('#imgCard').append(data[i]);
+//     }
+// }
+// display();
